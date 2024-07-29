65621f1c58cbe825ceac91ad3403181484190777
jerryshengg <158345382+jerryshengg@users.noreply.github.com>
2024-07-29 11:24:32 -0400

    Update genotype.sh

--- scripts/genotype.sh
+++ scripts/genotype.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# Load necessary modules
 module load GATK/4.5.0.0
-#for ii in {2..2}
-#do
-#gatk GenotypeGVCFs -R  /fdb/igenomes/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa -V gendb://gatk_ped/chr${ii}_gdb -O finalvcf/chr${ii}.vcf.gz
+module load picard
+module load bcftools
+
+# Define directories
+GENOME_FA="/fdb/igenomes/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa"
+GENDB_DIR="gatk_ped"
+FINAL_VCF_DIR="finalvcf"
+MERGED_VCF="${FINAL_VCF_DIR}/merged.vcf.gz"
+
+# Genotype GVCFs (uncomment and adjust as needed)
+#for ii in {1..22}; do
+#    gatk GenotypeGVCFs -R "$GENOME_FA" -V gendb://"$GENDB_DIR"/chr${ii}_gdb -O "$FINAL_VCF_DIR"/chr${ii}.vcf.gz
 #done
 
-module load picard
-java -jar $PICARDJARPATH/picard.jar GatherVcfs I=finalvcf/chr1.vcf.gz I=finalvcf/chr2.vcf.gz I=finalvcf/chr3.vcf.gz I=finalvcf/chr4.vcf.gz I=finalvcf/chr5.vcf.gz I=finalvcf/chr6.vcf.gz I=finalvcf/chr7.vcf.gz I=finalvcf/chr8.vcf.gz I=finalvcf/chr9.vcf.gz I=finalvcf/chr10.vcf.gz I=finalvcf/chr11.vcf.gz I=finalvcf/chr12.vcf.gz I=finalvcf/chr13.vcf.gz I=finalvcf/chr14.vcf.gz I=finalvcf/chr15.vcf.gz I=finalvcf/chr16.vcf.gz I=finalvcf/chr17.vcf.gz I=finalvcf/chr18.vcf.gz I=finalvcf/chr19.vcf.gz I=finalvcf/chr20.vcf.gz I=finalvcf/chr21.vcf.gz I=finalvcf/chr22.vcf.gz O=finalvcf/merged.vcf.gz
+# Gather VCF files
+VCF_INPUTS=$(for i in {1..22}; do echo "I=${FINAL_VCF_DIR}/chr${i}.vcf.gz"; done)
+java -jar $PICARDJARPATH/picard.jar GatherVcfs $VCF_INPUTS O="$MERGED_VCF"
 
-module load bcftools
-tabix -f -p vcf finalvcf/merged.vcf.gz
-
-
-
-
+# Index the merged VCF file
+tabix -f -p vcf "$MERGED_VCF"
